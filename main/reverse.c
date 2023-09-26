279694119137de5d9e6a8ef1efc4a8d3e7fa900c
Matheus Rodrigues <matheusrsr.cunha@gmail.com>
2023-09-26 17:33:03 -0300

    Feita a troca da logica e trabalhando na função para validar as entradas do terminal

--- main/reverse.c
+++ main/reverse.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 
-void ft_reverse_cripto(char* string) {
-    // Implemente a lógica da função ft_reverse_cripto aqui
+char	validacao ( char *string);
+
+int main(int argc, char* argv[]) 
+{	
+	int	i;
+	char	resto;
+
+	i = 0;
+	if (argc =! 3)
+	{
+		prinf(" Falta argumentos para a função ");
+	}
+	
+	while ( argc < 4)
+	{
+		i =+1 
+		
+		ft_validacao(argv[i]);
+		argc = i;
+	}
 }
 
-void ft_reverse_descripto(char* string) {
-    // Implemente a lógica da função ft_reverse_descripto aqui
-}
 
-int main(int argc, char* argv[]) {
-    
+char	ft_validacao(char *string)
+{
+	char	resto;
+	char	resto1;
+
+	while ( *string != '\0')
+	{
+		if ( *string < 'a' or *string > 'z')
+		{
+			if (*string < 'A' or *string > 'Z')
+			{
+				if (*string < '0' or *string > '9')
+				{
+					resto = "F";
+				}
+			}
+		}
+		string ++;
+	}
+	resto = "S";
