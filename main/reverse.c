37c3ac10c03d4d726859939f201c3485e4006d05
Matheus Rodrigues <matheusrsr.cunha@gmail.com>
2023-09-26 12:59:58 -0300

    foi identificado um erro de logica e esta sendo refeito o codigo para realizar a correção da main, antes da chamada da função cripto

--- main/reverse.c
+++ main/reverse.c
@@ -1,37 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int	main(int argc	char* argv[])
-{
-	int selecao_menu;
-	char string_cripto[50];
-	
-	selecao_menu = argv[1];
-	string_cripto = argv[2];
-
-	if (argc != 3)
-	{
-		printf("Erro 001")
-		return(1);
-	}
-
-	else
-	
-	{
-	
-			if (selecao_menu =="1")
-	
-			{
-				ft_reverse_cripto(string_cripto);
-			}
-	
-			else if (selecao_menu == "2")
-			{
-				ft_reverse_descripto(string_cripto);
-			}
-			else
-			{
-				printf("Erro 001");
-			}
-	}
-return(1);
+void ft_reverse_cripto(char* string) {
+    // Implemente a lógica da função ft_reverse_cripto aqui
 }
+
+void ft_reverse_descripto(char* string) {
+    // Implemente a lógica da função ft_reverse_descripto aqui
+}
+
+int main(int argc, char* argv[]) {
+    
